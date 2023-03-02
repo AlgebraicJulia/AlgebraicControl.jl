21633333ec58415cff28d3619b6eb0508abe021c
Tyler Hanks <teh.hanks@gmail.com>
2023-03-02 16:52:33 -0500

    Experiments

--- test/Optimizers.jl
+++ test/Optimizers.jl
@@ -6,42 +6,45 @@ using LinearAlgebra
 
 # Test optimization of an atomic problem
 vals = -5.0:5.0
-A = rand(vals, 3, 3)
-B = rand(vals, 3, 3)
-b = rand(vals, 3)
-Q = rand(vals, 3, 3)
-Q = Q'*Q
-r = rand(vals, 3)
+A = rand(2, 5)
+B = rand(5, 5)
+b = rand(vals, 2)
+Q = rand(vals, 5, 5)
+#Q = Q'*Q
+Q = I(5)
+r = rand(vals, 5)
 
 f(x) = x'*Q*x + r'*x
 g(x) = A*x - b
 
-p = IneqConstrainedProb(SmoothFunction(3,3,f), SmoothFunction(3,3,g))
+p = IneqConstrainedProb(SmoothFunction(5,5,f), SmoothFunction(5,5,g))
 o = OpenOptimizer(p)
 
-x_tst1 = forward(o, zeros(3); max_iters=10000)
-backward!(o, zeros(3), zeros(3); max_iters=10000)
+x_tst1 = forward(o, zeros(5); max_iters=10000)
+backward!(o, zeros(5), zeros(5); max_iters=10000)
 x_tst2 = primal_value(o)
-x_true = optimize(p, zeros(3); max_iters=10000)[1]
+x_true = optimize(p, zeros(5); max_iters=10000)[1]
 
 @test x_tst1 ≈ x_true
 @test x_tst2 ≈ x_true
 
 # Test a composite optimization
 f_sub(x) = x'*Q*x + r'*x
-f(x) = f_sub(x[1:3]) + f_sub(x[4:end])
-g1(x) = A*x-b
-g2(x, y) = B*y - x
-g(x) = vcat(g1(x[1:3]), g2(x[1:3],x[4:end]))
-true_p = IneqConstrainedProb(SmoothFunction(6, 1, f), SmoothFunction(6,6,g))
-true_x, true_μ = optimize(true_p, zeros(6); max_iters=100000)
+f(x) = f_sub(x[1:5]) + f_sub(x[6:end])
+h1(x) = A*x-b
+h2(x, y) = B*y - x
+h(x) = vcat(h1(x[1:5]), h2(x[1:5],x[6:end]))
+true_p = EqConstrainedProb(SmoothFunction(10, 1, f), SmoothFunction(10,10,h))
+println("Running Composite Optimization...")
+true_x, true_μ = optimize(true_p, zeros(10); max_iters=50000)
 
-p1 = IneqConstrainedProb(SmoothFunction(3,1,f_sub), SmoothFunction(3,3,g1))
-p2 = IneqConstrainedProb(SmoothFunction(3,1,f_sub), SmoothFunction(3,3,y->B*y))
+p1 = EqConstrainedProb(SmoothFunction(5,1,f_sub), SmoothFunction(5,5,h1))
+p2 = EqConstrainedProb(SmoothFunction(5,1,f_sub), SmoothFunction(5,5,y->B*y))
 
+println("Running Decomposed Optimization...")
 o = compose(OpenOptimizer(p1), OpenOptimizer(p2))
-for i in 1:10
-    backward!(o, zeros(3), zeros(3))
+for i in 1:5
+    backward!(o, zeros(5), zeros(5))
 end
 
 tst_x = vcat(primal_value(o.os[1]), primal_value(o.os[2]))
