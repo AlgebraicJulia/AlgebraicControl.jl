86903b4ff684df7b206ce82902d9f04eb7cb225d
tylerhanks <teh.hanks@gmail.com>
2023-05-17 18:23:10 -0400

    R_bar pixel arrays for MPC bifunctions

--- src/PixelArrays.jl
+++ src/PixelArrays.jl
@@ -1,6 +1,76 @@
 using Plots
 import Plots: plot
-import Base: *
+using LinearAlgebra
+import Base: *, +
+
+struct MPCBifunction
+    state_space::Int
+    control_space::Int
+    cost::Function # state_space × control_space → R
+    dynamics::Function # state_space × control_space → state_space
+end
+
+struct Pack
+    nports::Int # P
+    lb::Vector{Float64} # lb : P → R
+    ub::Vector{Float64} # ub : P → R
+    step_sizes::Vector{Float64}
+    resolution::Vector{Int} # resolution : P → N
+end
+function Pack(nports::Int, lb::Vector{Float64}, ub::Vector{Float64}, resolution::Vector{Int})
+    step_sizes = Float64[]
+    for i in 1:nports
+        push!(step_sizes, (ub[i] - lb[i]) / resolution[i])
+    end
+    return Pack(nports, lb, ub, step_sizes, resolution)
+end
+
+function entries(p::Pack)
+    ranges = [1:u for u in p.resolution]
+    return Iterators.product(ranges...)
+end
+
+
+struct Pixel
+    bounds::Vector{Pair{Float64}}
+end
+
+# Get the pixel of an entry as a list of lb=>ub pairs for each dimension
+function Pixel(p::Pack, e::Vector{Int})
+    result = Pair{Float64}[]
+    for i in 1:p.nports
+        push!(result, (p.lb[i] + p.step_sizes[i]*(e[i]-1)) => p.lb[i] + p.step_sizes[i]*e[i])
+    end
+    return Pixel(result)
+end
+
++(p1::Pair{Float64}, p2::Pair{Float64}) = (p1[1] + p2[1]) => (p1[2] + p2[2])
+
+function evaluate_pixel(p::Pixel, f::MPCBifunction; ϵ=0.1)
+    # Get centroid of pixel
+    centroid = Float64[]
+    for (lb,ub) in p.bounds
+        push!(centroid, (lb+ub)/2)
+    end
+    #println(centroid)
+    # Group centroid into inputs for the bifunction
+    x_in = centroid[1:f.state_space]
+    u = centroid[f.state_space+1:f.state_space+f.control_space]
+    x_out_desired = centroid[f.state_space+f.control_space+1:end]
+    x_out = f.dynamics(x_in, u)
+    if norm(x_out - x_out_desired) <= ϵ
+        return f.cost(x_in, u)
+    else
+        return Inf
+    end
+end
+
+struct PackUWD
+    dom::Vector{Pack}
+    codom::Pack
+    links::Pack
+    φ::Function
+end
 
 # Super naive implementations for now!
 struct PixelMatrix
@@ -23,6 +93,7 @@ function PixelMatrix(x_res, y_res, x_range, y_range, rel::Function)
             s4 = rel(x+x_step,y+y_step) < 0
             #println("Checking the relation on ($x,$y).")
             #m[i,j] = rel(x,y)
+            #m[i,j] = s1 != s4
             m[i,j] = s1 != s4 || s2 != s3
             #m[i,j] = s1 != s2 || s3 != s4
             #m[i,j] = s1 != s3 || s2 != s4
@@ -61,3 +132,16 @@ pm2 = PixelMatrix(100,100,-1.5=>1.5,-1.5=>1.5,r2)
 
 res = pm1*pm2
 plot(res)
+
+# Example bifunction usage
+f = MPCBifunction(1,1,(x,u)->x[1]^2 + u[1]^2, (x,u)->[5*x[1] + u[1]])
+p = Pack(3, repeat([-1.5],3), repeat([1.5],3), repeat([100],3))
+
+for e in entries(p)
+    pix = Pixel(p, collect(e))
+    val = evaluate_pixel(pix, f, ϵ=0.015)
+    if val != Inf
+        println(pix)
+        println(val)
+    end
+end
